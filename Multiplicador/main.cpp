31d4f6b81badcae234111401f92537802e9e9401
Davco-30 <davco.003@gmail.com>
2022-05-18 14:30:31 -0500

    Multiplicador C++

---
+++ Multiplicador/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Ingresa 2 números\n";
+    int num1;
+    int num2;
+
+    cin >> num1;
+    cin >> num2;
+
+    int mult = num1 * num2;
+    cout << mult;
+
+    return 0;
+}
