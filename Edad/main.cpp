70bbd5c2f488649123fb87589488c67e3cd086e0
Davco-30 <davco.003@gmail.com>
2022-05-19 14:18:08 -0500

    Verificador de edad

---
+++ Edad/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Por favor, oingrese la edad de la persona: \n" << endl;
+    int age;
+    cin >> age;
+
+    if(age < 18){
+        cout << "La persona es menor de edad.";
+    }
+
+    if(age >= 18){
+        cout << "La persona es mayor de edad.";
+    }
+
+    return 0;
+}
