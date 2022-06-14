3a1a8db284400fb44064f34c7c601b227006ec5e
Davco-30 <davco.003@gmail.com>
2022-06-14 13:05:43 -0500

    14 de junio

---
+++ Leccion_14_06/main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+///Funciones
+
+///Hacer una nueva función
+/// Tipo Nombre(Tipo de dato _ Variable == Parámetros){ Procedimiento }
+
+///Estas declaraciones permiten que main() pueda estar presente en el principio.
+int suma(int a, int b); ///Esta función declara al programa: "Se encontrará una función ENTERA llamada 'suma' con parámetros enteros de a hasta b"
+int resta(int a, int b); ///Esta función declara al programa: "Se encontrará una función ENTERA llamada 'resta' con parámetros enteros de a hasta b"
+int mult(int a, int b);
+float divide(float a, float b); ///Esta función declara al programa: "Se encontrará una función FLOTANTE llamada 'divide' con parámetros flotantes de a hasta b"
+float form_g(float a, float b, float c);
+float form_g_2(float a, float b, float c);
+
+
+int main(){  ///Cerebro del programa
+    cout << suma(8,7) << endl;
+    cout << resta(8,7) << endl;
+    cout << mult(8,7) << endl;
+    cout << divide(8,7) << endl;
+    cout << form_g(1, 0, -1) << endl;
+    cout << form_g_2(1, 0, -1) << endl;
+    return 0;
+}
+
+int f(int x){  ///Nueva función
+    return x * x;
+}
+
+int suma(int a, int b){
+    return a + b; ///return es la clave para regresar la operación que corresponde a la función 'suma'
+}
+
+int resta(int a, int b){
+    return a - b;
+}
+
+int mult(int a, int b){
+    return a * b;
+}
+
+float divide(float a, float b){
+    return a / b;
+}
+
+float form_g(float a, float b, float c){
+    return (-b + sqrt( (b * b) - (4*a*c)) ) / (2 * a);
+}
+
+float form_g_2(float a, float b, float c){
+    float result = (-b - sqrt( (b * b) - (4*a*c)) ) / (2 * a);
+    return result;
+}
+
--- Tarea4_08_06/main.cpp
+++ Tarea4_08_06/main.cpp
@@ -5,7 +5,106 @@ using namespace std;
 int main()
 {
     ///1
-    ///Leer n números y después leer un número k. Indicar cuantas y cuales parejas de números suman exactamente k.
-    cout << "Hello world!" << endl;
+    ///Leer n números y después leer un número k. Indicar cuantas parejas de números suman exactamente k.
+    ///Luego ingresa cuales parejas;
+
+    //Número de números
+    int num_1;
+    cout << "Ingresa un número" << endl;
+
+    cin >> num_1;
+
+    //Números del arreglo
+    int num_array[num_1];
+    cout << "\nAhora ingresa más números de acuerdo al primero que has dado.\n";
+
+    for(int i = 0; i < num_1; i++){
+        cin >> num_array[i];
+    }
+
+    //Número k
+    int num_k;
+    cout << "\nFinalmente, ingresa un último número" << endl;
+
+    cin >> num_k;
+
+    //Cantidad de números que dan k en la suma
+    int pair_total = 0;
+
+    //Este 'for' hace la suma d enúmeros para comprobar si dan exactamente k y suma 2 a la cantidad de números (o pares) que dan k
+    for(int i = 0; i < num_1; i++){
+        int sum = 0;
+
+        for(int j = i+1; j < num_1; j++){
+            sum = num_array[i] + num_array[j];
+
+            if(sum == num_k){
+                pair_total = pair_total + 2;
+            }
+        }
+
+    }
+
+    //Arreglo de los números
+    int pair_list[0];
+    int pair_list_size = 0;
+
+    for(int i = 0; i < num_1; i++){
+        int sum = 0;
+
+        for(int j = i + 1; j < num_1; j++){
+            sum = num_array[i] + num_array[j];
+
+            if(sum == num_k){
+                pair_list[pair_list_size] = num_array[i];
+                pair_list_size = pair_list_size + 1;
+                pair_list[pair_list_size] = num_array[j];
+                pair_list_size = pair_list_size + 1;
+            }
+        }
+
+    }
+
+
+    //Número de pares
+    int pair_quantity = pair_total / 2;
+
+    //Si el número de parejas es mayor a 1
+    if(pair_quantity > 1){
+        cout << "\nEl total de parejas de números que suman exactamente " << num_k << " es: " << pair_quantity << " parejas.\n";
+
+        cout << "\nLas parejas de números que suman exactamente " << num_k << " son: \n";
+
+        for(int i = 0; i < pair_list_size; i++){
+             cout << pair_list[i] << " ";
+        }
+
+        cout << "\n";
+    }
+
+
+    //Si el número de parejas es igual a 0
+    if(pair_quantity == 0){
+        cout << "\nEl total de parejas de números que suman exactamente " << num_k << " es: " << pair_quantity << " parejas.\n";
+        cout << "\n";
+    }
+
+
+    //Si el número de parejas es igual a 1
+    if(pair_quantity == 1){
+        cout << "\nEl total de parejas de números que suman exactamente " << num_k << " es: " << pair_quantity << " pareja.\n";
+
+        cout << "\nLa pareja de números que suman exactamente " << num_k << " es: \n";
+
+        for(int i = 0; i < pair_list_size; i++){
+             cout << pair_list[i] << " ";
+        }
+
+
+        cout << "\n";
+    }
+
+
+
     return 0;
 }
